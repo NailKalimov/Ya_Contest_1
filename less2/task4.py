9c5c53716837dfb36e37e4297b91aba7d773aff2
Nail <nailkalimov@gmail.com>
2024-11-15 22:08:49 +0400

    Add task 8 to the repository.

--- less2/task4.py
+++ less2/task4.py
@@ -3,17 +3,18 @@
 (такое, которое будет максимальным, если вычеркнуть из последовательности 
 одно максимальное число)"""
 
+
 def find_two_max(seq):
-    if seq[0]>seq[1]:
-        max1=seq[0]
-        max2=seq[1]
+    if seq[0] > seq[1]:
+        max1 = seq[0]
+        max2 = seq[1]
     else:
-        max1=seq[1]
-        max2=seq[0]
-    for i in range(2,len(seq)):
-        if seq[i]>max1:
-            max2=max1
-            max1=seq[i]
-        elif seq[i]>max2:
-            max2=seq[i]
-    return max1,max2
+        max1 = seq[1]
+        max2 = seq[0]
+    for i in range(2, len(seq)):
+        if seq[i] > max1:
+            max2 = max1
+            max1 = seq[i]
+        elif seq[i] > max2:
+            max2 = seq[i]
+    return max1, max2
